2637bf9f745f18808668d7c5592b924ac0816e75
radhika-rani <111755638+radhika-rani@users.noreply.github.com>
2024-08-07 11:31:11 +0530

    Delete Week2/LibraryManagement directory

--- Week2/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    // Method to simulate finding all books
-    public void findAllBooks() {
-        System.out.println("Finding all books...");
-    }
-}
