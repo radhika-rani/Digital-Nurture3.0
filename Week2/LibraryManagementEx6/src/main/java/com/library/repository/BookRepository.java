1a4a0a5af80dc18da1e23aefa93123b5d4de6e5d
radhika-rani <111755638+radhika-rani@users.noreply.github.com>
2024-08-07 10:51:43 +0530

    Add files via upload

---
+++ Week2/LibraryManagementEx6/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    public void findAllBooks() {
+        System.out.println("Finding all books...");
+    }
+}
