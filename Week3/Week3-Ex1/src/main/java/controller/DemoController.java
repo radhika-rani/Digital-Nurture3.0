d6291c58901d1641d66232954f3f842f7468a4f2
radhika-rani <111755638+radhika-rani@users.noreply.github.com>
2024-08-14 12:03:23 +0530

    Add files via upload

---
+++ Week3/Week3-Ex1/src/main/java/controller/DemoController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+// Annotation
+@Controller
+ 
+// Class
+public class DemoController {
+ 
+    @RequestMapping("/hello")
+    @ResponseBody
+ 
+    // Method
+    public String helloWorld()
+    {
+ 
+        // Print statement
+        return "Hello World!";
+    }
+}
