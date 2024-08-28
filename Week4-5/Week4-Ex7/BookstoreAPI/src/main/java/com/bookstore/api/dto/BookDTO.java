7f2cdeb7e91d5c2f1e661b050c7b91aa99989bc2
radhika-rani <111755638+radhika-rani@users.noreply.github.com>
2024-08-28 15:07:30 +0530

    Add files via upload

---
+++ Week4-5/Week4-Ex7/BookstoreAPI/src/main/java/com/bookstore/api/dto/BookDTO.java
@@ -0,0 +1,53 @@
+package com.bookstore.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+public class BookDTO {
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+    private String isbn;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
